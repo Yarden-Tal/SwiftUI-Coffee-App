091275fa2d890ff87dd4c9bbd444f743edf6b472
Yarden Tal <yarden.tal@quatro-digital.com>
2023-07-11 15:32:25 -0400

    initial structure

--- Coffee App/ContentView.swift
+++ Coffee App/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    var orders: [Int] = [1]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            ZStack {
+                
+                // Background img
+                Image("coffee").resizable().scaledToFill().opacity(0.5).grayscale(0.5).frame(width: .leastNormalMagnitude)
+                // Title & Subtitle
+                VStack {
+                    Text("Order Coffee!").font(.title).foregroundColor(.black).fontWeight(.heavy)
+                    Text("Fresh & delicious").font(.title2).foregroundColor(.gray).fontWeight(.heavy)
+                    // Cart icon
+                    Image(systemName: orders.isEmpty ? "cart" : "cart.circle.fill").padding(.vertical)
+                    // Order details
+                    ScrollView {
+                        ForEach(orders, id: \.self) { order in
+                            HStack {
+                                  Text("Your order item").foregroundColor(.white)
+                                  Spacer()
+                                Text(10.9, format: .currency(code: "USD")).foregroundColor(.white)
+                              }
+                        }
+                    // Items
+                    }
+                    ScrollView {
+                        HStack {
+                            Text("Espresso").foregroundColor(.white)
+                            Spacer()
+                            if let img = UIImage(named: "drink") {
+                                Image(uiImage: img).resizable().scaledToFit().frame(width: 50)
+                            }
+                            else {
+                                Image("fallback").resizable().scaledToFit().frame(width: 50).clipShape(Circle())
+                            }
+                        }
+                        Spacer()
+                    }
+                }.padding(.all).fontWeight(.bold)
+            }
     }
 }
 
