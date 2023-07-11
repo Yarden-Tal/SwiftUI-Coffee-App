4b62d593ace79393469bc6f455c3e7e44b3c50ec
Yarden Tal <yarden.tal@quatro-digital.com>
2023-07-11 11:12:54 -0400

    Initial Commit

---
+++ Coffee App/Coffee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_AppApp.swift
+//  Coffee App
+//
+//  Created by Yarden Tal on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
